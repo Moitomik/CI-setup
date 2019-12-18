d2a908ecf2d4fefc879f42cf0ea8efbb00ed68e5
Moitomik <toma-8907@mail.ru>
2019-12-18 15:31:21 +0300

    New code version

--- src/test/java/ru/netology/CashbackHackService/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackService/CashbackHackServiceTest.java
@@ -10,23 +10,17 @@ class CashbackHackServiceTest {
     void shouldNotCalculateCashbackIfAmountLowerThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
-
         int actualResult = service.remain(amount);
         int expectedResult = 200;
-
         assertEquals(expectedResult, actualResult);
-
         }
 
     @Test
     void shouldCalculateCashbackIfAmountIsOrMoreThan1000() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
-
         int actualResult = service.remain(amount);
         int expectedResult = 0;
-
         assertEquals(expectedResult, actualResult);
-
       }
 }
